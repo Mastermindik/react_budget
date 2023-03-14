5cf3bc3ed9b44f54bbd9c9c584a4406e334161e6
Danylo Dykhno <dykhnodanylo10@gmail.com>
2023-03-14 13:49:06 +0200

    deleted

--- src/components/PreLoader.js
+++
@@ -1,9 +0,0 @@
-function PreLoader() {
-  return <div class="preloader">
-  <div class="circle"></div>
-  <div class="circle"></div>
-  <div class="circle"></div>
-</div>
-}
-
-export default PreLoader
